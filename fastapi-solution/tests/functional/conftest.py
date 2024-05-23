dd1c9fcadcd8444d92b569f37443c7d75a10f667
Ilya Skryagin <infox182@gmail.com>
2024-05-23 15:59:25 +0300

    Use formatter

--- fastapi-solution/tests/functional/conftest.py
+++ fastapi-solution/tests/functional/conftest.py
@@ -29,30 +29,30 @@ async def list_in_parts(data: list):
         iter_count += 1
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
 
-@pytest_asyncio.fixture(scope='session')
+@pytest_asyncio.fixture(scope="session")
 async def es_client():
     client = AsyncElasticsearch(
-        hosts=f'{test_settings.es_host}:{test_settings.es_port}'
+        hosts=f"{test_settings.es_host}:{test_settings.es_port}"
     )
     yield client
     await client.close()
 
 
-@pytest_asyncio.fixture(scope='session')
+@pytest_asyncio.fixture(scope="session")
 async def http_session():
     sess = ClientSession()
     yield sess
     await sess.close()
 
 
-@pytest_asyncio.fixture(scope='session')
+@pytest_asyncio.fixture(scope="session")
 async def redis_client():
     sess = Redis(host=test_settings.redis_host, port=test_settings.redis_port)
     yield sess
@@ -65,20 +65,16 @@ def es_write_data(es_client: AsyncElasticsearch):
         if await es_client.indices.exists(index=index):
             await es_client.indices.delete(index, ignore_unavailable=True)
         await es_client.indices.create(
-            index=index,
-            body=test_settings.es_index_mapping[index])
+            index=index, body=test_settings.es_index_mapping[index]
+        )
         async for part in list_in_parts(data):
             bulk_data = []
             for elem in part:
-                elem_id = elem['id']
-                bulk_data.append({
-                        '_index': index,
-                        '_id': elem_id,
-                        '_source': elem
-                    }
-                )
+                elem_id = elem["id"]
+                bulk_data.append({"_index": index, "_id": elem_id, "_source": elem})
             await async_bulk(es_client, bulk_data)
             await asyncio.sleep(1)
+
     return inner
 
 
@@ -86,19 +82,21 @@ def es_write_data(es_client: AsyncElasticsearch):
 def es_clearing(es_client: AsyncElasticsearch):
     async def inner(index: str):
         await es_client.indices.delete(index, ignore_unavailable=True)
+
     return inner
 
 
 @pytest_asyncio.fixture
 def make_get_request(http_session: ClientSession):
     async def inner(path: str, params: dict = {}) -> RequestInfo:
-        api_path = 'api/v1/'
+        api_path = "api/v1/"
         url = os.path.join(test_settings.service_url, api_path, path)
         async with http_session.get(url, params=params) as response:
             body = await response.json(content_type=None)
             headers = response.headers
             status = response.status
         return body, headers, status
+
     return inner
 
 
@@ -109,6 +107,7 @@ def get_from_redis(redis_client: Redis):
         if data is None:
             return None
         return json.loads(data)
+
     return inner
 
 
@@ -116,6 +115,7 @@ def get_from_redis(redis_client: Redis):
 def redis_clearing(redis_client: Redis):
     async def inner() -> None:
         await redis_client.flushdb(asynchronous=True)
+
     return inner
 
 
@@ -124,47 +124,49 @@ def generate_films():
     def inner(n: int) -> list[dict]:
         es_data = [
             {
-                'id': str(uuid.uuid4()),
-                'imdb_rating': 8.5,
-                'creation_date': '2020-01-01',
-                'genres': [{'id': str(uuid.uuid4()), 'name': 'Action'}],
-                'title': 'The Star',
-                'description': 'New World',
-                'directors_names': ['Stan'],
-                'actors_names': ['Ann', 'Bob'],
-                'writers_names': ['Ben', 'Howard'],
-                'actors': [
-                    {'id': 'ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95', 'name': 'Ann'},
-                    {'id': 'fb111f22-121e-44a7-b78f-b19191810fbf', 'name': 'Bob'},
+                "id": str(uuid.uuid4()),
+                "imdb_rating": 8.5,
+                "creation_date": "2020-01-01",
+                "genres": [{"id": str(uuid.uuid4()), "name": "Action"}],
+                "title": "The Star",
+                "description": "New World",
+                "directors_names": ["Stan"],
+                "actors_names": ["Ann", "Bob"],
+                "writers_names": ["Ben", "Howard"],
+                "actors": [
+                    {"id": "ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95", "name": "Ann"},
+                    {"id": "fb111f22-121e-44a7-b78f-b19191810fbf", "name": "Bob"},
                 ],
-                'writers': [
-                    {'id': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'name': 'Ben'},
-                    {'id': 'b45bd7bc-2e16-46d5-b125-983d356768c6', 'name': 'Howard'},
+                "writers": [
+                    {"id": "caf76c67-c0fe-477e-8766-3ab3ff2574b5", "name": "Ben"},
+                    {"id": "b45bd7bc-2e16-46d5-b125-983d356768c6", "name": "Howard"},
                 ],
-                'directors': [
-                    {'id': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'name': 'Ben'},
-                    {'id': 'b45bd7bc-2e16-46d5-b125-983d356768c6', 'name': 'Howard'},
+                "directors": [
+                    {"id": "caf76c67-c0fe-477e-8766-3ab3ff2574b5", "name": "Ben"},
+                    {"id": "b45bd7bc-2e16-46d5-b125-983d356768c6", "name": "Howard"},
                 ],
             }
             for _ in range(n)
         ]
         return es_data
+
     return inner
 
 
 @pytest.fixture
 def make_normal_names():
     def inner(body: dict) -> dict:
-        body['uuid'] = body.pop('id')
-        for field in ['directors_names', 'actors_names', 'writers_names']:
+        body["uuid"] = body.pop("id")
+        for field in ["directors_names", "actors_names", "writers_names"]:
             if field in body:
                 body.pop(field)
-        for field in ['genres', 'directors', 'actors', 'writers']:
+        for field in ["genres", "directors", "actors", "writers"]:
             if field in body:
                 for i, v in enumerate(body[field]):
-                    if 'id' in v:
-                        body[field][i]['uuid'] = body[field][i].pop('id')
-                    if 'name' in v and field != 'genres':
-                        body[field][i]['full_name'] = body[field][i].pop('name')
+                    if "id" in v:
+                        body[field][i]["uuid"] = body[field][i].pop("id")
+                    if "name" in v and field != "genres":
+                        body[field][i]["full_name"] = body[field][i].pop("name")
         return body
+
     return inner
--- fastapi-solution/tests/functional/src/test_films.py
+++ fastapi-solution/tests/functional/src/test_films.py
@@ -11,43 +11,37 @@ from elasticsearch.helpers import async_bulk
 
 from tests.functional.settings import test_settings
 
+
 @pytest.mark.parametrize(
-    'query_data, expected_answer',
+    "query_data, expected_answer",
     [
-        (
-                {'query': 'The Star'},
-                {'status': 200, 'length': 50}
-        ),
-        (
-                {'query': 'Mashed potato'},
-                {'status': 200, 'length': 0}
-        ),
-        (
-                {'query': 'I am the Only One'},
-                {'status': 200, 'length': 1}
-        )
-    ]
+        ({"query": "The Star"}, {"status": 200, "length": 50}),
+        ({"query": "Mashed potato"}, {"status": 200, "length": 0}),
+        ({"query": "I am the Only One"}, {"status": 200, "length": 1}),
+    ],
 )
 @pytest.mark.asyncio
-async def test_search(generate_films,
-                      es_write_data,
-                      make_get_request,
-                      es_clearing,
-                      query_data,
-                      expected_answer):
+async def test_search(
+    generate_films,
+    es_write_data,
+    make_get_request,
+    es_clearing,
+    query_data,
+    expected_answer,
+):
 
     movie_index_name = test_settings.es_index_movie
-    
+
     # 1. Генерируем данные для ES
     es_data = generate_films(60)
-    
+
     single_movie = {
         "id": str(uuid.uuid4()),
         "imdb_rating": 8.5,
-        "title": 'I am the Only One',
+        "title": "I am the Only One",
     }
     es_data.append(single_movie)
-    
+
     # 2. Загружаем данные в ES
     await es_write_data(movie_index_name, es_data)
 
@@ -58,34 +52,33 @@ async def test_search(generate_films,
     try:
         assert status == expected_answer["status"]
         assert len(body) == expected_answer["length"]
-        if query_data['query'] == 'I am the Only One':
+        if query_data["query"] == "I am the Only One":
             single_movie["uuid"] = single_movie.pop("id")
             assert body[0] == single_movie
-        
+
     # 5. Удаляем индекс
     finally:
         await es_clearing(movie_index_name)
 
 
 @pytest.mark.parametrize(
-    'query_data, expected_answer',
+    "query_data, expected_answer",
     [
-        (
-                {'page_size': 100},
-                {'status': 200, 'length': 77}
-        ),
-    ]
-)  
-@pytest.mark.asyncio      
-async def test_all_films(generate_films,
-                         es_write_data,
-                         make_get_request,
-                         es_clearing,
-                         query_data,
-                         expected_answer):
+        ({"page_size": 100}, {"status": 200, "length": 77}),
+    ],
+)
+@pytest.mark.asyncio
+async def test_all_films(
+    generate_films,
+    es_write_data,
+    make_get_request,
+    es_clearing,
+    query_data,
+    expected_answer,
+):
 
     movie_index_name = test_settings.es_index_movie
-    
+
     es_data = generate_films(77)
 
     await es_write_data(movie_index_name, es_data)
@@ -98,31 +91,29 @@ async def test_all_films(generate_films,
 
     finally:
         await es_clearing(movie_index_name)
-        
 
 
 @pytest.mark.parametrize(
-    'query_data, expected_answer',
+    "query_data, expected_answer",
     [
-        (
-                {},
-                {'status': 200}
-        ),
-    ]
-)  
-@pytest.mark.asyncio      
-async def test_single_film(generate_films,
-                           make_normal_names,
-                           es_write_data,
-                           get_from_redis,
-                           make_get_request,
-                           es_clearing,
-                           redis_clearing,
-                           query_data,
-                           expected_answer):
+        ({}, {"status": 200}),
+    ],
+)
+@pytest.mark.asyncio
+async def test_single_film(
+    generate_films,
+    make_normal_names,
+    es_write_data,
+    get_from_redis,
+    make_get_request,
+    es_clearing,
+    redis_clearing,
+    query_data,
+    expected_answer,
+):
 
     movie_index_name = test_settings.es_index_movie
-    
+
     es_data = generate_films(1)
     single_movie = es_data[0]
     uuid_movie = single_movie["id"]
@@ -130,21 +121,23 @@ async def test_single_film(generate_films,
     try:
         # 1. Загружаем данные в ES
         await es_write_data(movie_index_name, es_data)
-        
+
         # 2. Проверяем пустой ли redis
-        redis_value = await get_from_redis(f'{movie_index_name}:{uuid_movie}')
+        redis_value = await get_from_redis(f"{movie_index_name}:{uuid_movie}")
         assert redis_value is None
-        
+
         # 3. Запрашиваем данные из ES по API
-        body, headers, status = await make_get_request(f"films/{uuid_movie}", query_data)
-        
+        body, headers, status = await make_get_request(
+            f"films/{uuid_movie}", query_data
+        )
+
         # 4. Проверяем cтатус
         assert status == expected_answer["status"]
-        
+
         # 5. Изменяем названия полей для проверки
         single_movie = make_normal_names(single_movie)
         # 6. Запрашиваем данные из redis
-        redis_value = await get_from_redis(f'{movie_index_name}:{uuid_movie}')
+        redis_value = await get_from_redis(f"{movie_index_name}:{uuid_movie}")
         # 7. Проверяем ответ из ES
         assert body == single_movie
         # 7. Проверяем ответ из redis
