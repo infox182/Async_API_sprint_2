4b8268154e421567016fb6e181786132ae85921c
Ilya Skryagin <infox182@gmail.com>
2024-05-24 15:42:22 +0300

    Add backoff

--- fastapi-solution/tests/functional/utils/wait_for_es.py
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 import time
+import backoff
 
 from elasticsearch import Elasticsearch
 
@@ -8,14 +9,24 @@ sys.path.append("/tests/functional")
 
 from settings import test_settings
 
+
+@backoff.on_exception(
+    backoff.expo,
+    Exception,
+    max_tries=test_settings.max_tries,
+    max_time=test_settings.max_time,
+)
+def es_ping(es_client):
+    if es_client.ping():
+        print("ES Работает")
+        return None
+    raise Exception
+
+
 if __name__ == "__main__":
     es_client = Elasticsearch(
         hosts=[f"{test_settings.es_host}:{test_settings.es_port}"],
         validate_cert=False,
         use_ssl=False,
     )
-    while True:
-        if es_client.ping():
-            print("ES Работает")
-            break
-        time.sleep(1)
+    es_ping(es_client)
--- fastapi-solution/tests/functional/utils/wait_for_redis.py
+++ fastapi-solution/tests/functional/utils/wait_for_redis.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 import time
+import backoff
 
 from redis import Redis
 
@@ -8,10 +9,20 @@ sys.path.append("/tests/functional")
 
 from settings import test_settings
 
+
+@backoff.on_exception(
+    backoff.expo,
+    Exception,
+    max_tries=test_settings.max_tries,
+    max_time=test_settings.max_time,
+)
+def redis_ping(redis_client):
+    if redis_client.ping():
+        print("Redis Работает")
+        return None
+    raise Exception
+
+
 if __name__ == "__main__":
     redis_client = Redis(host=test_settings.redis_host, port=test_settings.redis_port)
-    while True:
-        if redis_client.ping():
-            print("Redis Работает")
-            break
-        time.sleep(1)
+    redis_ping(redis_client)
